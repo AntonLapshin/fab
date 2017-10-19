6ba513162d1696ae5b7a87075016d8cbdbca6920
Anton Lapshin <antvallap@gmail.com>
2017-10-19 19:55:45 +0200

    Updates

--- fab.es6.js
+++ fab.es6.js
@@ -4,7 +4,7 @@
 	(factory((global.fab = global.fab || {})));
 }(this, (function (exports) { 'use strict';
 
-const COMMON_CSS = ".fab-wrap{margin:25px;position:fixed;z-index:999999;right:0;bottom:0}.fab-wrap>a{margin-bottom:0 !important}.fab-wrap a{text-decoration:none;height:52px;width:52px;line-height:52px;display:block;border-radius:50%;box-shadow:0 0 3px rgba(0,0,0,0.15),0 4px 8px rgba(0,0,0,0.28);cursor:pointer;position:relative;text-align:center;transition:all ease 0.2s;margin-bottom:8px}.fab-wrap a:hover:after{opacity:1}.fab-wrap a>svg{position:absolute;margin:auto;left:0;right:0;top:0;bottom:0;display:inline-block}.fab-wrap a+.fab-btns{visibility:hidden;position:absolute;bottom:100%;animation:0.5s out;padding-bottom:20px;margin-bottom:-20px}.fab-wrap a:after{content:attr(data-fab-label);box-sizing:border-box;font-size:14px;position:absolute;pointer-events:none;z-index:-1;transition:all ease 0.2s;opacity:0;border-radius:9px;padding:0 50px 0 1em;text-align:left;right:50%;line-height:52px}.fab-wrap:hover a+.fab-btns{visibility:visible;animation:0.5s in}@keyframes in{0%{opacity:0;transform:translate(0px, -25px)}100%{opacity:1;transform:translate(0px, 0px)}}@keyframes out{0%{opacity:1;transform:translate(0px, 0px)}100%{opacity:0;transform:translate(0px, -25px)}}";
+const COMMON_CSS = ".fab-wrap{margin:25px;position:fixed;z-index:999999;right:0;bottom:0}.fab-wrap>a{margin-bottom:0 !important}.fab-wrap a{text-decoration:none;height:52px;width:52px;line-height:52px;display:block;border-radius:50%;box-shadow:0 0 3px rgba(0,0,0,0.15),0 4px 8px rgba(0,0,0,0.28);cursor:pointer;position:relative;text-align:center;transition:all ease 0.2s;margin-bottom:8px}.fab-wrap a:hover:after{opacity:1}.fab-wrap a>svg{position:absolute;margin:auto;left:0;right:0;top:0;bottom:0;display:inline-block}.fab-wrap a+.fab-btns{visibility:hidden;position:absolute;bottom:100%;animation:0.5s out;padding-bottom:20px;margin-bottom:-20px}.fab-wrap a:after{content:attr(data-fab-label);box-sizing:border-box;font-size:14px;position:absolute;pointer-events:none;z-index:-1;transition:all ease 0.2s;opacity:0;border-radius:9px;padding:0 50px 0 1em;text-align:left;right:50%;line-height:52px}.fab-wrap:hover a+.fab-btns,.fab-wrap.hovered a+.fab-btns{visibility:visible;animation:0.5s in}@keyframes in{0%{opacity:0;transform:translate(0px, -25px)}100%{opacity:1;transform:translate(0px, 0px)}}@keyframes out{0%{opacity:1;transform:translate(0px, 0px)}100%{opacity:0;transform:translate(0px, -25px)}}";
 
 const THEME = {
   bgColor: "#0083ca",
@@ -73,7 +73,7 @@ const uid = () => "fab_" + _uid++;
 
 const attachCallback = btn => {
   btn.fn &&
-    document.getElementById(btn.id).addEventListener("mousedown", e => {
+    document.getElementById(btn.id).addEventListener("click", e => {
       btn.fn();
       e.stopPropagation();
     });
@@ -95,6 +95,9 @@ const create = (btn, theme) => {
   attachStyle(getTheme(id, theme));
 
   const el = document.getElementById(btn.id).parentNode;
+  el.addEventListener("click", e => {
+    el.target.classList.toggle("hovered");
+  });
   return {
     hide: () => {
       el.style.display = "none";
